6ec2b5dfb9d13b631ad15a46eeafdb7e63eb4e9c
0xDanki <typepuller@gmail.com>
2023-07-24 04:35:11 +0800

    Added escrow functionality

--- README.md
+++ README.md
@@ -1,36 +1,113 @@
-# OpenMarket dApp
-OpenMarket is a decentralized marketplace that allows public market vendors and wholesalers to buy directly from local farmers. The local farmers can list their harvest to expedite the process of finding a buyer for their produce.
+# OpenMarket
+OpenMarket is a decentralized agricultural marketplace that allows public market vendors and wholesalers to buy directly from local farmers. The local farmers can list their harvest to expedite the process of finding a buyer for their produce.
 
-## Libraries
+This documentation provides an overview of the OpenMarket, its features, and the technologies used in its development.
+
+## Table of Contents
+
+- [What Problem We Solve](#what-problem-we-solve)
+- [Our Solution](#introduction)
+- [Userflow](#userflow)
+- [Libraries](#libraries)
+- [Future Developments](#future-developments)
+- [Dev Guide](#dev-guide)
+
+## What Problem We Solve
+
+There is a huge inefficiency in the country’s agricultural supply chain as we find many consumers willing to buy more produce yet farmers are struggling to get paid a fair price because of too many middlemen. By the time their products reach the market, their price has tripled or even quadrupled from what the farmers had sold them for. 
+
+## Introduction
+
+OpenMarket is a decentralized ecommerce app that allows public market vendors and wholesalers buy directly from local farmers and track the full cycle of their order, from planting to distribution.
+
+Our app will enable farmers to sell directly to wholesalers, get paid a fair price, and conveniently have them delivered to the public market. On the buyer side, people who buy raw crops from Openmarket get to enjoy huge discounts that they may not find elsewhere.
+
+As a side benefit, putting farmers’ produce on a public listing prevents wastage and enables complete traceability of the crops, making it harder to smuggle agricultural products in the country. If implemented widely, it will also enable monitoring of the incoming supply and demand for crops and make coordinated planting (and procuring farm inputs) possible.
+
+## Userflow
+
+We have two main users in OpenMarket, the Farmer and the Buyer. Each has a simple userflow.
+
+Userflow for Farmer:
+
+1. Connects Wallet and Registers as a Farmer
+2. Uploads Crops with Estimated Date
+3. Accept or Reject an offer
+4. Ship the Product
+5. Get Paid
+
+
+Userflow for Buyer:
+
+1. Connects Wallet and Browses for Crops
+2. Sends an Offer
+3. Escrows the payment when the offer is accepted
+4. Receives the crops 
+
+
+## Technologies Used
+
+OpenMarket uses the following Frameworks, Libraries, and Technology:
+
+**Smart Contract Development**: Hardhat and Solidity, Openzeppelin
+
+**Testing and Deploying**: EthersJS and Mocha Framework
+
+**Frontend**: ReactJS, NextJS, and TailwindCSS
+
+**Backend**: Polybase (A decentralized database)
+
+**Marketplace Currency**: USDC
+
+
+
+## Future Development
+
+OpenMarket is an ongoing project, and I will keep adding features and fixes to improve the platform. But the upcoming features are:
+
+* Deployment on AVAX Subnet, a special blockchain meant especially for the smooth running of the dApp
+* Putting escrowed payments on yield-bearing vaults
+* Integration of Zero-Knowledge (ZK) technology to enhance user privacy
+* Improvements on User Interface for better mobile experience
+* Implementation of advanced search and filtering capabilities
+
+
+## Dev Guide
+
+### Libraries
 
 Initializable
 Reentrancy Guard
+Polybase
 
-## State Variables
+### State Variables
 
 1. Item struct
 2. Order struct
 3. Farmer struct
-4. farmers mapping
-5. items mapping
+4. Farmers mapping
+5. Items mapping
 6. Orders per user mapping
 7. Private variables for counters
 
-## Modifiers
+### Modifiers
 
 1. onlyOwner
 2. onlyFarmer
 
-## Functions
+### Functions
 
 1. addFarmer - Registers a farmer. Only farmers can list an item
 2. list - creates a new Item and adds it to the items mapping. Only farmers can call this function
 3. buy (payable) - buy an item. Open to external addresses. Fetches an item, creates an order, add this order to the user, subtracts from item stock, pays the seller.
 	- require that msg.value is greater than cost of item
 	- require that item is in stock
-4. deleteItem - delists an item so it can no longer be bought. Only the seller of the item can call this function.
+4. cancel - cancels an order
+5. setShipping - sets and requests shipment fee
+6. payShipping - pays the farmer the shipment fee
+7. deleteItem - delists an item so it can no longer be bought. Only the seller of the item can call this function.
 
-## Unit Test Coverage (as of Feb 10, 2023)
+### Unit Test Coverage (as of Feb 10, 2023)
 
 Deployment <br>
     ✔ Sets the owner<br>
--- contracts/Openmarket.sol
+++ contracts/Openmarket.sol
@@ -8,8 +8,8 @@ contract Openmarket is Initializable, ReentrancyGuard {
     address public owner;
 
     //will have to add harvest date and status to this struct sometime later. eg. enum Status {Planted, Harvested, Processed, Warehouse, Transit}
-    //will have to add a function to change the listing once the harvest date and status is added to this struct. As for now, we assume that the item is already harvested and ready to be bought
-    struct Item {        
+    //will have to add a function to change the listing once the harvest date and status is added to this struct. As for now, we assume that the listing is already harvested and ready to be bought
+    struct Listing {        
         uint256 id;
         address payable seller;
         string name;
@@ -21,33 +21,47 @@ contract Openmarket is Initializable, ReentrancyGuard {
         uint256 stock;
     }
 
+    struct Escrow {
+        address payable buyer;
+        address payable seller;
+        uint256 amount;
+        bool released;
+        bool refunded;
+    }
+
+    enum OrderStatus { Placed, Shipped, Delivered, Refunded }
+
     struct Order {
         uint256 time;
-        Item item;
-        string buyerAddr;
+        Listing listing;
+        address buyerAddr;
         string phone;
+        OrderStatus status;
+        bool shipped;
     }
 
     struct Farmer {
         uint id;
         string name;
         string city;
-        string barangay;
+        string locality;
         bool isRegistered;
     }
 
     mapping(address => Farmer) public farmers;
-    mapping(uint256 => Item) public items;
+    mapping(uint256 => Listing) public listings;
     mapping(address => mapping(uint256 => Order)) public orders;
     mapping(address => uint256) public orderCount;
+    mapping(uint256 => uint256) public escrow; // Order ID to escrowed amount
+    mapping(uint256 => Escrow) public escrows;
     uint256 private farmerCount;
-    uint256 private itemsCount;
+    uint256 private listingsCount;
 
 
-    event FarmerRegistered (uint id, string name, string city, string barangay);
-    event Buy(address buyer, uint256 orderId, uint256 itemId, string buyerAddr, string phone);
-    event List(uint itemId, address seller, string sellerName, string name, string category, uint256 cost, string unit, uint256 stock);
-    event Delisted(uint256 itemId, string name);
+    event FarmerRegistered (uint id, string name, string city, string locality);
+    event Buy(address buyer, uint256 orderId, uint256 listingId, address buyerAddr, string phone);
+    event List(uint listingId, address seller, string sellerName, string name, string category, uint256 cost, string unit, uint256 stock);
+    event Delisted(uint256 listingId, string name);
 
     modifier onlyOwner() {
         require(msg.sender == owner);
@@ -59,25 +73,31 @@ contract Openmarket is Initializable, ReentrancyGuard {
         _;
     }
 
+    modifier onlyBuyer(uint256 _orderId) {
+        Order storage order = orders[msg.sender][_orderId];
+        require(msg.sender == order.buyerAddr, "You are not the buyer of this order");
+        _;
+    }
+
     function initialize() public initializer {
         owner = msg.sender;
     }
 
-    function addFarmer(string memory _name, string memory _city, string memory _barangay) public {
+    function addFarmer(string memory _name, string memory _city, string memory _locality) public {
         //Makes sure the required fields are filled out
         require(bytes(_name).length > 0);
         require(bytes(_city).length > 0);
-        require(bytes(_barangay).length > 0);
+        require(bytes(_locality).length > 0);
 
         //Increments the farmer count and creates a new Farmer in the mappings with the msg.sender address as its ID
         farmerCount ++;
-        farmers[msg.sender]=Farmer(farmerCount, _name, _city, _barangay, true);
+        farmers[msg.sender]=Farmer(farmerCount, _name, _city, _locality, true);
 
-        emit FarmerRegistered(farmerCount, _name, _city, _barangay);
+        emit FarmerRegistered(farmerCount, _name, _city, _locality);
     }    
 
 
-    //UX consideration: do we really need the farmer to upload an image? If there's a way to display an image based on the item name in the frontend...
+    //UX consideration: do we really need the farmer to upload an image? If there's a way to display an image based on the listing name in the frontend...
     function list(
         string memory _name,
         string memory _category,
@@ -88,11 +108,11 @@ contract Openmarket is Initializable, ReentrancyGuard {
         uint256 _stock
     ) external onlyFarmer {
 
-        //Increments the items count and creates a new Item in the mappings with the itemsCount as its ID and msg.sender as its seller
-        itemsCount ++;
+        //Increments the listings count and creates a new listing in the mappings with the listingsCount as its ID and msg.sender as its seller
+        listingsCount ++;
         address payable _seller = payable(msg.sender);
-        items[itemsCount]=Item(
-            itemsCount, 
+        listings[listingsCount]=Listing(
+            listingsCount, 
             _seller, 
             _name, 
             _category, 
@@ -102,53 +122,89 @@ contract Openmarket is Initializable, ReentrancyGuard {
             _rating, 
             _stock);
 
-        emit List(itemsCount, _seller, farmers[_seller].name, _name, _category, _cost, _unit, _stock);
+        emit List(listingsCount, _seller, farmers[_seller].name, _name, _category, _cost, _unit, _stock);
     }
 
-    //room for improvement: the msg.value will go first to an escrow instead of going straight to the creator's wallet
-    //i'm not sure if putting nonReentrant is useful in an external function, but yeah i'm paranoid like that lol
-    function buy(uint256 _id, string memory _buyerAddr, string memory _phone) external payable nonReentrant {
-        // Fetch item
-        Item storage item = items[_id];
+    function initiateEscrow(uint256 _orderId, address _buyer, address _seller, uint256 _amount) internal {
+        Escrow storage escrowData = escrows[_orderId];
+        escrowData.buyer = payable(_buyer); // Explicit typecast to address payable
+        escrowData.seller = payable(_seller); // Explicit typecast to address payable
+        escrowData.amount = _amount;
+        escrowData.released = false;
+        escrowData.refunded = false;
+    }
 
-        // Require enough ether to buy item
-        require(msg.value >= item.cost, "Please submit the asking price in order to complete the purchase");
+    function releaseEscrow(uint256 _orderId) internal onlyFarmer {
+        Escrow storage escrowData = escrows[_orderId];
+        require(!escrowData.released, "Escrow already released");
+        escrowData.released = true;
+        payable(escrowData.seller).transfer(escrowData.amount);
+    }
 
-        // Require item is in stock
-        require(item.stock > 0, "Item is out of stock");
+    function refundBuyer(uint256 _orderId) internal onlyFarmer {
+        Escrow storage escrowData = escrows[_orderId];
+        require(!escrowData.released && !escrowData.refunded, "Escrow already released or refunded");
+        escrowData.refunded = true;
+        payable(escrowData.seller).transfer(escrowData.amount);
+    }
+
+    function buy(uint256 _id, address _buyerAddr, string memory _phone) external payable nonReentrant {
+        // Fetch listing
+        Listing storage listing = listings[_id];
+
+        // Require enough ether to buy listing
+        require(msg.value >= listing.cost, "Please submit the asking price in order to complete the order");
+
+        // Require listing is in stock
+        require(listing.stock > 0, "Listing is out of stock");
 
         // Create order
-        Order memory order = Order(block.timestamp, item, _buyerAddr, _phone);
+        Order memory order = Order(block.timestamp, listing, _buyerAddr, _phone, OrderStatus.Placed, false);
 
-        //Transfers fund from buyer to seller
-        item.seller.transfer(item.cost);
-
+        // Initiate escrow
+        initiateEscrow(orderCount[msg.sender], msg.sender, listing.seller, listing.cost);
+       
         // Add order for user
-        orderCount[msg.sender]++; // <-- Order ID
+        orderCount[msg.sender]++;
         orders[msg.sender][orderCount[msg.sender]] = order;
 
-        // Subtract stock
-        item.stock --;
-
-        // Emit event
-        emit Buy(msg.sender, orderCount[msg.sender], _id, _buyerAddr, _phone);
+       // Emit event
+       emit Buy(msg.sender, orderCount[msg.sender], _id, _buyerAddr, _phone);
+    }
+    
+    function markAsShipped(uint256 _orderId) external onlyFarmer {
+        Order storage order = orders[msg.sender][_orderId];
+        require(order.status == OrderStatus.Placed, "Order must be in exist");
+        order.status = OrderStatus.Shipped;
+        order.shipped = true;
     }
 
-    function deleteItem(uint256 _id) external nonReentrant {
+    function confirmReceipt(uint256 _orderId) external onlyBuyer(_orderId) {
+        Order storage order = orders[msg.sender][_orderId];
+        require(order.status == OrderStatus.Shipped, "Order must be in 'Shipped' status to confirm receipt");
 
-        //Fetch item
-        Item storage item = items[_id];
+        // Mark order as delivered
+        order.status = OrderStatus.Delivered;
 
-        //Require that the item is listed and has stock
-        require(item.stock > 0, "Item must be on the market");
+        // Release escrow to the seller
+        releaseEscrow(_orderId);
+    }
+
+    function deleteListing(uint256 _id) external nonReentrant {
+
+        //Fetch listing
+        Listing storage listing = listings[_id];
+
+        //Require that the listing is listed and has stock
+        require(listing.stock > 0, "Listing must be on the market");
 
         //Require that the function caller is the product seller
-        require(item.seller == msg.sender, "You must be the product owner ");
+        require(listing.seller == msg.sender, "You must be the product owner ");
 
-        //sets the item stock to zero
-        item.stock = 0;
+        //sets the listing stock to zero
+        listing.stock = 0;
         
-        emit Delisted(_id, item.name);
+        emit Delisted(_id, listing.name);
   }
 
 
